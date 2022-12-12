6d6637e399b00db22b831338b2a52d14672760a3
Florian Gebele <florian@gebele.net>
2022-12-12 23:59:38 +0100

    12

--- 12/index.ts
+++ 12/index.ts
@@ -2,7 +2,7 @@ let start: Point = { x: 0, y: 0 };
 let end: Point = { x: 0, y: 0 };
 const grid = new Map<string, Node>();
 
-const data = Deno.readTextFileSync("12/input.txt.org")
+Deno.readTextFileSync("12/input.txt")
   .split("\r\n")
   .map((line, y) =>
     line
@@ -17,12 +17,26 @@ const data = Deno.readTextFileSync("12/input.txt.org")
         }
         return c.charCodeAt(0) % 97;
       })
-      .forEach((lev, x) =>
-        grid.set(toKey(x, y), { height: lev, x, y, neighbourKeys: [] })
-      )
+      .forEach((lev, x) => {
+        grid.set(toKey(x, y), {
+          height: lev,
+          x,
+          y,
+          neighbours: [],
+          path: [],
+          h: Number.MAX_SAFE_INTEGER,
+          g: Number.MAX_SAFE_INTEGER,
+        });
+      })
   );
-function toKey(x: number, y: number) {
-  return `${x}-${y}`;
+
+function toKey(x: number, y: number): string;
+function toKey(p: Point): string;
+function toKey(a: any, b?: any) {
+  if (typeof b === "number") {
+    return `${a}-${b}`;
+  }
+  return `${a.x}-${a.y}`;
 }
 type Point = {
   x: number;
@@ -31,18 +45,18 @@ type Point = {
 
 type Node = Point & {
   height: number;
-  h?: number;
-  neighbourKeys: Array<[string, number]>;
+  h: number;
+  g: number;
+  neighbours: Array<Node>;
+  path: Array<Node>;
 };
 
-// h(n) = h'(n, w1) + distance(w1, w2) + h'(w2, goal)
 const D = 1;
 function heuristic(node: Point, goal: Point) {
   const dx = Math.abs(node.x - goal.x);
   const dy = Math.abs(node.y - goal.y);
   return D * (dx + dy);
 }
-const optimalDistance: number = heuristic(start, end);
 
 [...grid.entries()].map(([key, val]) => {
   const left = toKey(val.x - 1, val.y);
@@ -51,28 +65,83 @@ const optimalDistance: number = heuristic(start, end);
   const down = toKey(val.x, val.y - 1);
   const isWalkable = (node: Node, keys: Array<string>) =>
     keys.map((key) =>
-      grid.has(key) &&
-      Math.abs((grid.get(key) as Node).height - node.height) < 2
+      grid.has(key) && (grid.get(key) as Node).height - node.height <= 1
         ? key
         : undefined
     );
-  val.neighbourKeys.push(
+  val.neighbours.push(
     ...(
       isWalkable(val, [left, right, top, down]).filter(
         (v) => !!v
       ) as Array<string>
-    ).map(
-      (key) =>
-        [
-          key,
-          heuristic(start, val) + 1 + heuristic(grid.get(key) as Node, end),
-        ] as [string, number]
-    )
+    ).map((key) => {
+      const n = grid.get(key) as Node;
+      n.h = heuristic(n, end);
+      return n;
+    })
   );
 });
 
-// distance f(n) = g(n) + h(n)
-function Search(start: Point, end: Point, map: Map<string, Node>) {
-  const openList = [];
-  const D = 1;
+class PriorityOrder {
+  list: Array<Node> = [];
+  constructor() {}
+  get() {
+    return this.list.shift();
+  }
+  put(node: Node) {
+    if (this.list.find((n) => node === n)) return;
+    const prio = node.g + node.h;
+    const putIndex = this.list.findIndex((n) => n.h + n.g > prio);
+    if (putIndex === -1) {
+      this.list.push(node);
+    } else {
+      const removedNodes = this.list.splice(putIndex);
+      this.list.push(node, ...removedNodes);
+    }
+  }
 }
+
+function anUglyStar(start: Point, grid: Map<string, Node>) {
+  grid.get(toKey(start))!.g = 0;
+  const closedList = new Set<string>();
+  const order = new PriorityOrder();
+  let endNode;
+  order.put(grid.get(toKey(start)) as Node);
+  while (true) {
+    const node = order.get();
+    if (!node) break;
+    if (node.x === end.x && node.y === end.y) {
+      endNode = node;
+    }
+    node.neighbours.forEach((n) => {
+      const checkG = node.g + 1;
+      if (n.g > checkG) {
+        n.g = checkG;
+        n.path = [...node.path, node];
+      }
+      if (!closedList.has(toKey(n))) order.put(n);
+    });
+    closedList.add(toKey(node));
+  }
+
+  return endNode;
+}
+
+function cleanGrid() {
+  [...grid.entries()].forEach(([key, node]) =>
+    grid.set(key, { ...node, g: Number.MAX_SAFE_INTEGER, path: [] })
+  );
+  return grid;
+}
+
+console.log(anUglyStar(start, grid)?.path.length);
+
+console.log(
+  [...grid.values()]
+    .filter((n) => n.height === 0)
+    .map((n) => anUglyStar(n, cleanGrid())?.path.length)
+    .map((l) => (l ? l : Number.MAX_SAFE_INTEGER))
+    .sort((a, b) => a - b)[0]
+);
+
+console.log();
